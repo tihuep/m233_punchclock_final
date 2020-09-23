747f74be765f6e22d48a2b53ad22f0fe7f618c9c
tihuep <44672849+tihuep@users.noreply.github.com>
2020-09-23 15:01:20 +0200

    adds files

---
+++ src/main/java/ch/zli/m223/punchclock/PunchclockApplication.java
@@ -0,0 +1,20 @@
+package ch.zli.m223.punchclock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+public class PunchclockApplication {
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(PunchclockApplication.class, args);
+	}
+
+}
